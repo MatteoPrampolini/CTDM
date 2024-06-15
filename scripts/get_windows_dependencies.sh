6751ac1f8e57dd60ab9ca8c216481aaef19a9796
marcosti <marcosti096@duck.com>
2024-06-15 14:31:20 +0200

    update dependency fetcher

--- scripts/get_windows_dependencies.sh
+++ scripts/get_windows_dependencies.sh
@@ -9,10 +9,10 @@ MPV_MD5="9844e3279dca2cd946388abe7742498f"
 FFMPEG_SHA256="2b4eaf0959f58b154d236c034a3daf3a1e40f1fec815d6644391bbffe841f828"
 
 curl -L -o mpv.7z https://sourceforge.net/projects/mpv-player-windows/files/release/mpv-0.38.0-x86_64.7z/download
-curl -L -o ffmpeg.7z https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-full.7z
+curl -L -o ffmpeg.7z https://github.com/GyanD/codexffmpeg/releases/download/7.0/ffmpeg-7.0-full_build.7z
 
-[[ "$MPV_SHA1" == "$(sha1sum mpv.7z | cut -d ' ' -f 1)" ]] || ( echo "Could not verify mpv.7z"; exit 1 )
-[[ "$MPV_MD5" == "$(md5sum mpv.7z | cut -d ' ' -f 1)" ]] || ( echo "Could not verify mpv.7z"; exit 1 )
-[[ "$FFMPEG_SHA256" == "$(sha256sum ffmpeg.7z | cut -d ' ' -f 1)" ]] || ( echo "Could not verify ffmpeg.7z"; exit 1 )
+[[ "$MPV_SHA1" == "$(sha1sum mpv.7z | cut -d ' ' -f 1)" ]] || ( echo "Could not verify mpv.7z (sha1: $(sha1sum mpv.7z | cut -d ' ' -f 1))"; exit 1 )
+[[ "$MPV_MD5" == "$(md5sum mpv.7z | cut -d ' ' -f 1)" ]] || ( echo "Could not verify mpv.7z (md5: $(md5sum mpv.7z | cut -d ' ' -f 1))"; exit 1 )
+[[ "$FFMPEG_SHA256" == "$(sha256sum ffmpeg.7z | cut -d ' ' -f 1)" ]] || ( echo "Could not verify ffmpeg.7z (sha256: $(sha256sum ffmpeg.7z | cut -d ' ' -f 1))"; exit 1 )
 
 echo "Successfully downloaded dependencies"
